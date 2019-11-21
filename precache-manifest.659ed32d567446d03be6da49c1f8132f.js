c84e553e728565c7b06c625f41a5254a726cfd24
Fender <fenderroads@mail.ru>
2019-11-21 11:47:36 +0400

    Updates

---
+++ precache-manifest.659ed32d567446d03be6da49c1f8132f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fede3d08d20c164cc55ec6c618edebb",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "c9633b01fa7f93d89129",
+    "url": "/react-app/static/css/main.e33fc3aa.chunk.css"
+  },
+  {
+    "revision": "3fd0a42b4090bd361f51",
+    "url": "/react-app/static/js/2.d7a60ebe.chunk.js"
+  },
+  {
+    "revision": "c9633b01fa7f93d89129",
+    "url": "/react-app/static/js/main.a0bd394c.chunk.js"
+  },
+  {
+    "revision": "b3db92340af72223c254",
+    "url": "/react-app/static/js/runtime-main.3082fc65.js"
+  }
+]);
